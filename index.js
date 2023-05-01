a9c9a2edc19da07aaf41f59e3bd1e75a8ded7b9f
asefibrahim <strongman01666@gmail.com>
2023-05-02 04:23:20 +0600

    Set data by routes

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ app.use(cors())
 app.get('/', (req, res) => {
     res.send('Server is running well....')
 })
+
+const data = require('./data/data.json')
+app.get('/chefs', (req, res) => {
+    res.send(data)
+})
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id
+    const selectedChef = data.find(d => d.id == id)
+    res.send(selectedChef)
+})
+
+
 app.listen(port)
